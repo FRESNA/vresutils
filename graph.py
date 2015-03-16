309b6f7d32935eb018746e4f9a58c3883c286f98
Sarah Becker <becker@fias.uni-frankfurt.de>
2015-03-16 17:32:44 +0100

    new function to calculate (N-1) critical links; some reordering

--- graph.py
+++ graph.py
@@ -30,26 +30,75 @@ def giant_component(G, copy=True):
     else:
         return g
 
-def minimum_spanning_tree(G):
-    """ Given a spatially embedded graph <G>, find the minimum spanning tree
-    using the node distances as link weights. Returns the minimum spanning tree
-    graph g with node distances as link weights. If returndist=True, also returns 
-    distance matrix (as dense array) between all nodes.
+def BreadthFirstLevels(G, root):
+    """ Generator of sets of 1-neighbours, 2-neighbours, ... k-neighbours
+    of node `root` in topology of `G`.
+    """
+
+    visited = set()
+    currentLevel = set((root,))
+    while currentLevel:
+        yield currentLevel
+        visited.update(currentLevel)
+        currentLevel = set(w
+                           for v in currentLevel
+                           for w in G[v]
+                           if w not in visited)
+
+def NodeListBFS(G, root, depth=10):
+    """ Return array of 1, 2, ..., <depth>th neighbours of root node <root> 
+    in graph <G>.
+    """
+
+    levels = []
+    nodes = []
+    if G.nodes() != range(G.number_of_nodes()):
+        root = G.nodes().index(root)
+        G = nx.convert_node_labels_to_integers(G)
+
+    for level, nodesoflevel in izip(xrange(depth), BreadthFirstLevels(G, root)):
+        levels += [level] * len(nodesoflevel)
+        nodes  += nodesoflevel
+
+    return np.array(levels), np.array(nodes)
+
+def get_distance_matrix(G):
+    """ Given a spatially embedded graph <G>, get the node positions and
+    calculate the pairwise euclidean distance of all nodes.
     """
     
     if G.nodes() != range(G.number_of_nodes()):
         G = nx.convert_node_labels_to_integers(G)
 
-    pos = nx.get_node_attribute(G, 'pos')
+    pos = nx.get_node_attributes(G, 'pos')
     pos = [pos[i] for i in range(G.number_of_nodes())]
 
-    # calculate distances 
-    # TODO these matrix formats are probably inefficient
+    # TODO these matrix formats are probably inefficient 
+    # (but at least understood across different packages!)
     distance_matrix = distance.pdist(pos, metric='euclidean')
     distance_matrix = distance.squareform(distance_matrix)
 
+    return distance_matrix
+
+
+def minimum_spanning_tree(G, distance_matrix=None):
+    """ Given a graph <G> and some underlying metric encoded in
+    <distance_matrix> (defaults to euclidean distance, which requires nodes
+    to have a position), find the minimum spanning tree using the node distances
+    as link weights. Returns the minimum spanning tree graph g with node
+    distances as link weights. If returndist=True, also returns distance matrix
+    (as dense array) between all nodes.
+    """
+    
+    if G.nodes() != range(G.number_of_nodes()):
+        G = nx.convert_node_labels_to_integers(G)
+
+    # calculate distances 
+    if distance_matrix == None:
+        distance_matrix = get_distance_matrix(G)
+
     # calculate minimum spanning tree
-    span_tree = sparse.csgraph(distance_matrix, overwrite=True)
+    span_tree = sparse.csgraph.minimum_spanning_tree(distance_matrix, overwrite=True)
 
     # translate back to graph edges and add them to g, store distance in edge weight
     span_tree = sparse.coo_matrix(span_tree)
@@ -60,6 +109,30 @@ def minimum_spanning_tree(G):
                               for n, m, weight in zip(span_tree.row, span_tree.col, span_tree.data))
 
     return g
+
+def find_N_minus_one_critical_links(G, edges=None):
+    """ Given a graph <G>, find the critical links whose removal leads to 
+    network decomposition into multiple components.
+    Optionally, restrict the search to edge list <edges>.
+
+    Brute force for now, better ideas welcome.
+    """
+
+    if nx.number_connected_components(G) > 1:
+        return G.edges()
+
+    if edges == None:
+        edges = G.edges()
+
+    critical_links = []
+    for edge in edges:
+        print edge
+        G.remove_edge(*edge)
+        if nx.number_connected_components(G) > 1:
+            critical_links.append(edge)
+        G.add_edge(*edge)
+
+    return critical_links
 
 def cell_subgraph(G, lat, lon, size):
     """ Returns cutout of G with node positions around a point described by 
@@ -102,38 +175,6 @@ def polygon_subgraph(G, polygon, nneighbours=0):
                         if not n in nodelist)
 
     return giant_component(g)
-
-def BreadthFirstLevels(G, root):
-    """ Generator of sets of 1-neighbours, 2-neighbours, ... k-neighbours
-    of node `root` in topology of `G`.
-    """
-
-    visited = set()
-    currentLevel = set((root,))
-    while currentLevel:
-        yield currentLevel
-        visited.update(currentLevel)
-        currentLevel = set(w
-                           for v in currentLevel
-                           for w in G[v]
-                           if w not in visited)
-
-def NodeListBFS(G, root, depth=10):
-    """ Return array of 1, 2, ..., <depth>th neighbours of root node <root> 
-    in graph <G>.
-    """
-
-    levels = []
-    nodes = []
-    if G.nodes() != range(G.number_of_nodes()):
-        root = G.nodes().index(root)
-        G = nx.convert_node_labels_to_integers(G)
-
-    for level, nodesoflevel in izip(xrange(depth), BreadthFirstLevels(G, root)):
-        levels += [level] * len(nodesoflevel)
-        nodes  += nodesoflevel
-
-    return np.array(levels), np.array(nodes)
 
 def voronoi_partition(G):
     """ For 2D-embedded graph <G>, returns the shapes of the Voronoi cells 
