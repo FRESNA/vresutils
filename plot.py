e095a036030e28c8f92b852fa780466efb3a5bdb
Jonas Hoersch <jonas@chaoflow.net>
2015-03-19 21:35:42 +0100

    plot: Add draw_edges routine

--- plot.py
+++ plot.py
@@ -1,5 +1,12 @@
+import numpy as np
+import networkx as nx
+
 import matplotlib as mpl
 from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+import matplotlib.cbook as cb
+from matplotlib.colors import colorConverter, Colormap
+from matplotlib.collections import LineCollection
 
 from . import make_toModDir
 toModDir = make_toModDir(__file__)
@@ -25,3 +32,128 @@ def germany(resolution='l', ax=None, meta=None):
     m.drawcoastlines()
     m.drawcountries()
     return m
+
+def draw_edges(G, segments, pos=None, edgelist=None, width=1.0, color='k',
+               style='solid', alpha=None, ax=None, **kwds):
+    """Draw the edges of the graph G.
+
+    This draws the edge segments given by a separation of the links in
+    `data` of the graph G.
+
+    Parameters
+    ----------
+    G : graph
+       A networkx graph
+
+    segments : L x M array
+       The segmentation of each link. (segments.sum(axis=1) == 1).all()
+
+    pos : dictionary
+       A dictionary with nodes as keys and positions as values.
+       Positions should be sequences of length 2.
+       (default=nx.get_node_attributes(G, 'pos'))
+
+    edgelist : collection of edge tuples
+       Draw only specified edges(default=G.edges())
+
+    width : float or array of floats
+       Line width of edges (default =1.0)
+
+    color : tuple of color strings
+       Edge Segments color. Can be a single color format string (default='r'),
+       or a sequence of colors with the same length as data.shape[1].
+
+    style : string
+       Edge line style (default='solid') (solid|dashed|dotted,dashdot)
+
+    alpha : float
+       The edge transparency (default=1.0)
+
+    ax : Matplotlib Axes object, optional
+       Draw the graph in the specified Matplotlib axes.
+
+    Returns
+    -------
+    matplotlib.collection.LineCollection
+        `LineCollection` of the edge segments
+
+    """
+    if not np.allclose(segments.sum(axis=1), 1):
+        segments = segments / segments.sum(axis=1)[:,np.newaxis]
+
+    if ax is None:
+        ax = plt.gca()
+
+    if pos is None:
+        pos = nx.get_node_attributes(G, 'pos')
+
+    if edgelist is None:
+        edgelist = G.edges()
+
+    if not edgelist or len(edgelist) == 0:  # no edges!
+        return None
+
+    if not cb.iterable(width):
+        lw = (width,)
+    else:
+        lw = width
+
+    if cb.iterable(color) \
+           and len(color) == segments.shape[1]:
+        if np.alltrue([cb.is_string_like(c) for c in color]):
+            # (should check ALL elements)
+            # list of color letters such as ['k','r','k',...]
+            edge_colors = tuple([colorConverter.to_rgba(c, alpha) for c in color])
+        elif (np.alltrue([not cb.is_string_like(c) for c in color])
+              and np.alltrue([cb.iterable(c) and len(c) in (3, 4) for c in color])):
+            edge_colors = tuple(color)
+        else:
+            raise ValueError('color must consist of either color names or numbers')
+    else:
+        if cb.is_string_like(color) or len(color) == 1:
+            edge_colors = (colorConverter.to_rgba(edge_color, alpha), )
+        else:
+            raise ValueError('color must be a single color or list of exactly m colors where m is the number of segments')
+
+    assert len(edgelist) == segments.shape[0], "Number edges and segments have to line up"
+
+    # set edge positions
+    edge_pos = np.asarray([(pos[e[0]], pos[e[1]]) for e in edgelist])
+
+    src = edge_pos[:,0]
+    dest = edge_pos[:,1]
+
+    positions = src[:,np.newaxis] + np.cumsum(np.hstack((np.zeros((len(segments), 1)), segments)), axis=1)[:,:,np.newaxis]*(dest - src)[:,np.newaxis]
+
+    linecolls = []
+    for s in range(segments.shape[1]):
+        coll = LineCollection(positions[:,s:s+2],
+                              colors=edge_colors[s:s+1],
+                              linewidths=lw,
+                              antialiaseds=(1,),
+                              linestyle=style,
+                              transOffset = ax.transData)
+
+        coll.set_zorder(1)  # edges go behind nodes
+        # coll.set_label(label)
+
+        if cb.is_numlike(alpha):
+            coll.set_alpha(alpha)
+
+        ax.add_collection(coll)
+        linecolls.append(coll)
+
+    # update view
+    minx = np.amin(np.ravel(edge_pos[:, :, 0]))
+    maxx = np.amax(np.ravel(edge_pos[:, :, 0]))
+    miny = np.amin(np.ravel(edge_pos[:, :, 1]))
+    maxy = np.amax(np.ravel(edge_pos[:, :, 1]))
+
+    w = maxx-minx
+    h = maxy-miny
+    padx,  pady = 0.05*w, 0.05*h
+    corners = (minx-padx, miny-pady), (maxx+padx, maxy+pady)
+    ax.update_datalim(corners)
+    ax.autoscale_view()
+
+    return linecolls
