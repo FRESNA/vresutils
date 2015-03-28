87659a1750bc8b592c8354daf8c3935434217283
Jonas Hoersch <jonas@chaoflow.net>
2015-03-29 00:15:15 +0100

    graph: Add polygon_subgraph_environment

--- graph.py
+++ graph.py
@@ -3,12 +3,13 @@
 
 import numpy as np
 import networkx as nx
-from itertools import izip, islice
+from itertools import izip, islice, chain
 from shapely.geometry import Point, Polygon
 from scipy.spatial import Voronoi
 from scipy.spatial import distance
 from scipy import sparse
-
+from collections import OrderedDict
+from scipy.linalg import norm
 
 def to_directed(G):
     """
@@ -202,6 +203,113 @@ def polygon_subgraph(G, polygon, nneighbours=0, copy=True):
                                               1, 1+nneighbours)))
 
     return giant_component(G.subgraph(nodes), copy=copy)
+
+
+def polygon_subgraph_environment(G, polygon, environment_polygons):
+    """
+    Return the subgraph induced on nodes within shapely Polygon. Nodes
+    covered by environment_polygons will be consolidated to one node
+    each and combine all the previous links to its bunch. The node
+    attributes length, X and Y are filled in again. All the nodes,
+    which have been subsumed into one supernode are accessible through
+    its node attribute 'nodes'.
+
+    Parameters
+    ----------
+    G : Graph
+        A spatially embedded graph with the node attribute pos and
+        edge attributes length and X.
+
+    polygon : shapely.geometry.Polygon
+        The region from where to keep nodes
+
+    environment_polygons : dict(region_name=Polygon)
+        For each Polygon all nodes within are collectively replaced by
+        one node, which unifies also all the previous links.
+
+    Returns
+    -------
+    G : Graph
+    """
+
+    H = G.__class__()
+    queue = OrderedDict()
+
+    def add_link(n, m, d):
+        if n != m:
+            H.adj[n][m] = d
+            H.adj[m][n] = d
+
+    def add_node(n):
+        if n in queue:
+            return queue[n]
+
+        attr_dict = G.node[n]
+        pos = Point(attr_dict['pos'])
+        if polygon.contains(pos):
+            # in Polygon: keep it
+            H.adj[n] = {}
+            H.node[n] = attr_dict
+            return n
+        else:
+            # this two-step procedure checks the last successful
+            # polygon (env_poly) first, as we are link-hopping
+            # through the graph
+            if not add_node.env_poly.contains(pos):
+                for name, p in environment_polygons.iteritems():
+                    if p.contains(pos):
+                        add_node.env_name = name
+                        add_node.env_poly = p
+                        break
+                else:
+                    # The point is not in any polygon => throw it
+                    return None
+            # n is in env_poly
+            if add_node.env_name not in H:
+                H.adj[add_node.env_name] = {}
+                H.node[add_node.env_name] = {'pos': np.asarray(add_node.env_poly.centroid),
+                                             'nodes': {n: attr_dict}}
+            else:
+                H.node[add_node.env_name]['nodes'][n] = attr_dict
+            return add_node.env_name
+    add_node.env_name, add_node.env_poly = next(environment_polygons.iteritems())
+
+    # nodes are added to done, as soon as all its adjoining neighbours
+    # and links have been added.
+    done = set()
+    for n in G:
+        if n in done: continue
+
+        a = add_node(n)
+        if a is None: continue
+        queue[n] = a
+
+        while queue:
+            n, a = queue.popitem()
+            for m, d in G.adj[n].iteritems():
+                if m in done: continue
+
+                b = add_node(m)
+                if b is None: continue
+                add_link(a, b, d)
+                queue[m] = b
+            done.add(n)
+
+    for env in environment_polygons:
+        # estimate X / length ratio
+        x = np.mean([m['X'] / m['length']
+                     for m in chain(H.adj[env].itervalues(),
+                                    (a for n in H.node[env]['nodes']
+                                       for a in G.adj[n].itervalues()))])
+        pos = H.node[env]['pos']
+        for n, attr in H.adj[env].iteritems():
+            length = norm(pos - H.node[n]['pos'])
+            X = x * length
+            attr.update(length=length, X=X, Y=1./X)
+
+    # graph attributes
+    H.graph = G.graph
+    return H
 
 
 def voronoi_partition(G, outline):
