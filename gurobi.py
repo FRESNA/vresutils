67197f55554434bd46f9e02225aa2682003fc09d
Jonas Hoersch <jonas@chaoflow.net>
2015-06-03 13:37:32 +0200

    gurobi: gbdot(GbVecExpr, GbVecVar) support

--- gurobi.py
+++ gurobi.py
@@ -223,7 +223,7 @@ def gbdot(v1, v2):
     elif isvectorlike(v1) and isinstance(v2, GbVecVar):
         assert len(v1) == len(v2)
         return gb.LinExpr(v1, v2)
-    elif isinstance(v1, (GbVecVar, GbVecExpr)) and isinstance(v2, GbVecExpr):
+    elif isinstance(v1, (GbVecVar, GbVecExpr)) and isinstance(v2, (GbVecVar, GbVecExpr)):
         # a faster implementation is probably possible from within
         # GbVecExpr but this pedestrian one should be good enough
         # TODO : Needs to be tested
