1a05ea92e2cf9fd1894005a4f748ab6d035fe76c
Jonas Hoersch <jonas@chaoflow.net>
2016-02-08 15:12:13 +0100

    decorators: Add a verbose flag to timer for suppressing output

--- decorators.py
+++ decorators.py
@@ -158,45 +158,52 @@ class timer(object):
     level = 0
     opened = False
 
-    def __init__(self, name=""):
+    def __init__(self, name="", verbose=True):
         self.name = name
+        self.verbose = verbose
 
     def __enter__(self):
-        if self.opened:
-            sys.stdout.write("\n")
+        if self.verbose:
+            if self.opened:
+                sys.stdout.write("\n")
 
-        if len(self.name) > 0:
-            sys.stdout.write((".. " * self.level) + self.name + ": ")
-        sys.stdout.flush()
+            if len(self.name) > 0:
+                sys.stdout.write((".. " * self.level) + self.name + ": ")
+            sys.stdout.flush()
+
+            self.__class__.opened = True
 
         self.__class__.level += 1
-        self.__class__.opened = True
 
         self.start = time.time()
         return self
 
+    def print_usec(self, usec):
+        if usec < 1000:
+            print("%.1f usec" % usec)
+        else:
+            msec = usec / 1000
+            if msec < 1000:
+                print("%.1f msec" % msec)
+            else:
+                sec = msec / 1000
+                print("%.1f sec" % sec)
+
+
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if not self.opened:
+        if not self.opened and self.verbose:
             sys.stdout.write(".. " * self.level)
 
         if exc_type is None:
             stop = time.time()
-            usec = (stop - self.start) * 1e6
-            if usec < 1000:
-                print("%.1f usec" % usec)
-            else:
-                msec = usec / 1000
-                if msec < 1000:
-                    print("%.1f msec" % msec)
-                else:
-                    sec = msec / 1000
-                    print("%.1f sec" % sec)
-        else:
+            self.usec = usec = (stop - self.start) * 1e6
+            if self.verbose: self.print_usec(usec)
+        elif self.verbose:
             print("failed")
         sys.stdout.flush()
 
         self.__class__.level -= 1
-        self.__class__.opened = False
+        if self.verbose: self.__class__.opened = False
         return False
 
 class optional(object):
