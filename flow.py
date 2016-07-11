619f8700b0077992453544b399b2fee9ad3a2082
Jonas Hoersch <jonas@chaoflow.net>
2016-07-12 00:01:52 +0200

    flow: Add the ability to change the order of the nodes in the PTDF

--- flow.py
+++ flow.py
@@ -6,8 +6,10 @@ import scipy as sp, scipy.sparse
 
 from .array import densify, spdiag
 
-def PTDF(G, susceptance='Y'):
-    lap = densify(nx.laplacian_matrix(nx.Graph(G), nodelist=G.nodes(),
+def PTDF(G, susceptance='Y', nodelist=None):
+    if nodelist is None:
+        nodelist = G.nodes()
+    lap = densify(nx.laplacian_matrix(nx.Graph(G), nodelist=nodelist,
                                       weight=susceptance)) # shallow undirected copy
-    K = nx.incidence_matrix(G, weight=susceptance, oriented=True)
+    K = nx.incidence_matrix(G, nodelist=nodelist, weight=susceptance, oriented=True)
     return np.asarray(- K.T.dot(np.linalg.pinv(lap)))
