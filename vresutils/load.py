71659bf601fccd3ec4cfb54eb143c293da06802a
Jonas Hoersch <jonas.hoersch@posteo.de>
2017-08-03 20:44:44 +0200

    load: Add load upsampling already from load.europe

--- vresutils/load.py
+++ vresutils/load.py
@@ -27,10 +27,15 @@ import pandas as pd
 import os
 import glob
 import pytz, datetime
+from scipy.optimize import leastsq
+from six.moves import range, zip
 
+from . import shapes as vshapes, mapping as vmapping, transfer as vtransfer
 from .decorators import cachable
+
 from . import make_toDataDir
 toDataDir = make_toDataDir(__file__)
+
 
 @cachable(keepweakref=True)
 def timeseries_entsoe(years=list(range(2011, 2015+1)), countries=None, directory=None):
@@ -159,3 +164,120 @@ def timeseries_opsd(years=slice("2011", "2015"), countries=None):
         load['EE'] = load['EE'].interpolate()
 
     return load
+
+def _upsampling_fitfunc(weights, gdp, pop):
+    return weights[0] * gdp + weights[1] * pop
+
+def _upsampling_weights(load):
+    """
+    Fit the weights for gdp and pop using leastsq from
+    the some load data for each country in europe.
+
+    Parameters
+    ----------
+    load : pd.DataFrame (index=times, columns=ISO2 country codes)
+
+    Returns
+    -------
+    weights : np.array((gdp, pop), dtype=np.float)
+    """
+
+    load = load.resample('AS').sum()
+    if (load.iloc[0] < 0.1 * load.iloc[1]).all():
+        # Year is not complete
+        load = load.iloc[1:]
+
+    def read_eurostat(fn, extradims=[]):
+        data = pd.read_csv(toDataDir(fn), thousands=' ', na_values=':')
+        data = data.set_index(['TIME', 'GEO'] + extradims).unstack()['Value']
+        data = data.unstack(list(range(-len(extradims), 0)))
+        data.set_index(pd.to_datetime(data.index, format="%Y"), inplace=True)
+        return data
+
+    def reindex_like_load(data, load):
+        data = data.stack().reindex(load.columns, level=1).unstack()
+        data = data.reindex(load.index)
+        data.interpolate('time', inplace=True)
+        data.bfill(inplace=True)
+
+        return data
+
+    gdp = reindex_like_load(read_eurostat('nama_10_gdp_1_Data.csv', ['NA_ITEM']), load)
+    pop = reindex_like_load(read_eurostat('demo_gind_1_Data.csv'), load)
+
+    def normed(x):
+        return x.divide(x.sum(axis=1), axis=0)
+
+    data = pd.Panel(dict(gdp=normed(gdp['Gross domestic product at market prices']),
+                         # gdpva=normed(gdp['Value added, gross']),
+                         pop=normed(pop),
+                         load=normed(load)))
+
+    data.dropna(axis=2, inplace=True)
+
+    gdp_n = np.ravel(data["gdp"])
+    pop_n = np.ravel(data["pop"])
+    y = np.ravel(data["load"])
+
+    Jerr = - np.hstack((gdp_n[:,np.newaxis],
+                        pop_n[:,np.newaxis]))
+    def errfunc(weights, gdp, pop):
+        return y - _upsampling_fitfunc(weights, gdp, pop)
+
+    weights, cov_x, infodict, mesg, ier = \
+    leastsq(errfunc, np.array((0.5, 0.5)), Dfun=lambda x,_,__: Jerr,
+            args=(gdp_n, pop_n), full_output=True)
+
+    return weights / weights.sum()
+
+def gdppop_nuts3():
+    pop = pd.read_table(toDataDir('nama_10r_3popgdp.tsv.gz'), na_values=[':'], delimiter=' ?\t', engine='python')
+    pop = (pop
+           .set_index(pd.MultiIndex.from_tuples(pop.pop('unit,geo\\time').str.split(','))).loc['THS']
+           .applymap(lambda x: pd.to_numeric(x, errors='coerce'))
+           .fillna(method='bfill', axis=1))['2014']
+
+    gdp = pd.read_table(toDataDir('nama_10r_3gdp.tsv.gz'), na_values=[':'], delimiter=' ?\t', engine='python')
+    gdp = (gdp
+           .set_index(pd.MultiIndex.from_tuples(gdp.pop('unit,geo\\time').str.split(','))).loc['EUR_HAB']
+           .applymap(lambda x: pd.to_numeric(x, errors='coerce'))
+           .fillna(method='bfill', axis=1))['2014']
+
+    # Swiss data
+    cantons = pd.read_csv(toDataDir('ch_cantons.csv'))
+    cantons = cantons.set_index(cantons['HASC'].str[3:])['NUTS']
+    swiss = pd.read_excel(toDataDir('je-e-21.03.02.xls'), skiprows=3, index_col=0)
+    swiss.set_axis('columns', swiss.columns.to_series().map(cantons))
+
+    pop = pop.append(pd.to_numeric(swiss.loc['Residents in 1000', 'CH04':]))
+    gdp = gdp.append(pd.to_numeric(swiss.loc['Gross domestic product per capita in Swiss francs', 'CH04':]))
+
+    return gdp, pop
+
+
+def timeseries_shapes(shapes, countries, years=slice("2011", "2015"), weights=None, load=None):
+    if load is None:
+        load = timeseries_opsd(years, countries.unique())
+
+    if weights is None:
+        weights = _upsampling_weights(load=load)
+
+    gdp, pop = gdppop_nuts3()
+    nuts3 = pd.Series(vshapes.nuts3(tolerance=None, minarea=0.))
+    mapping = vmapping.countries_to_nuts3()
+
+    def normed(x): return x.divide(x.sum())
+
+    def upsample(cntry, group):
+        l = load[cntry]
+        if len(group) == 1:
+            return pd.DataFrame({group.index[0]: l})
+        else:
+            nuts3_inds = mapping.index[mapping == cntry]
+            transfer = vtransfer.Shapes2Shapes(group, nuts3.reindex(nuts3_inds), normed=False).T.tocsr()
+            gdp_n = pd.Series(transfer.dot(gdp.reindex(nuts3_inds, fill_value=1.).values), index=group.index)
+            pop_n = pd.Series(transfer.dot(pop.reindex(nuts3_inds, fill_value=1.).values), index=group.index)
+            factors = normed(_upsampling_fitfunc(weights, normed(gdp_n), normed(pop_n)))
+            return pd.DataFrame(factors.values * l.values[:,np.newaxis], index=l.index, columns=factors.index)
+    return pd.concat([upsample(cntry, group)
+                      for cntry, group in shapes.groupby(countries)], axis=1)
